a47114fb0c34c011895ceb7e9a3ec7473824c003
Gabriela Werneck de Alencar <C3007893@fgv.edu.br>
2025-08-13 11:37:14 -0300

    Funçao cumprimento e chamada com o nome

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 print ("Hello world")
+
+def cumprimento(texto):
+    return f"Ola, {texto}"
+
+if __name__ == "__main__":
+    nome_completo = "Gabriela Werneck" 
+    print (cumprimento(nome_completo))

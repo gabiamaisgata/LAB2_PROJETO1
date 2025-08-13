1bb2d335454678fbde4ea707c423ea6522c2c2d9
Gabriela Werneck de Alencar <C3007893@fgv.edu.br>
2025-08-13 11:43:49 -0300

    Funçao que sorteia 7 numeros e calcula a media

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 print ("Hello world")
 
+import random
+
 def cumprimento(texto):
     return f"Ola, {texto}"
+
+def media_7_numeros():
+    nums = [random.randint(1,100) for _ in range (7)]
+    media = sum(nums) / len (nums)
+    return nums, media
 
 if __name__ == "__main__":
     nome_completo = "Gabriela Werneck" 
     print (cumprimento(nome_completo))
+    nums, media = media_7_numeros()
+    print ("Numeros sorteados:", nums)
+    print ("Media:", media)
